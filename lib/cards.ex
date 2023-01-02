139cf105f9f6a4a86c2c04863d4519c10d3c85c2
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-01-02 07:37:00 +0300

    Added save and load

--- lib/cards.ex
+++ lib/cards.ex
@@ -25,10 +25,9 @@ defmodule Cards do
 
     end
 
-
-
-
   end
+
+
 
   def shuffle(deck) do
     Enum.shuffle(deck)
@@ -43,6 +42,23 @@ defmodule Cards do
     Enum.split(deck , hand_size)
 
   end
+
+  def save(deck , filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename ,  binary)
+  end
+
+  def load(filename) do
+    {status , binary} = File.read(filename)
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "This file does not exist"
+
+
+    end
+  end
+
+
 
 
 
